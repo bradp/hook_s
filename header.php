1c5706e8547e4efd2fbf1d23b94dd4413bd0203b
Brad Parbs <brad@bradparbs.com>
2013-03-08 14:03:31 -0600

    Added header theme hooks

--- header.php
+++ header.php
@@ -8,8 +8,10 @@
  * @since _s 1.0
  */
 ?><!DOCTYPE html>
+<?php tha_html_before(); ?>
 <html <?php language_attributes(); ?>>
 <head>
+<?php tha_head_top(); ?>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
@@ -19,13 +21,17 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 
+<?php tha_head_bottom(); ?>
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+<?php tha_body_top(); ?>
 <div id="page" class="hfeed site">
+	<?php tha_header_before(); ?>
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header" role="banner">
+		<?php tha_header_top(); ?>
 		<hgroup>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
@@ -37,6 +43,8 @@
 
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
+		<?php tha_header_bottom(); ?>
 	</header><!-- #masthead -->
+	<?php tha_header_after(); ?>
 
 	<div id="main" class="site-main">
