5db1ad075c1a01524e83b63db98311baef8dc44e
Brad Parbs <brad@bradparbs.com>
2013-03-08 14:05:55 -0600

    Added footer theme hooks

--- footer.php
+++ footer.php
@@ -10,17 +10,20 @@
 ?>
 
 	</div><!-- #main -->
-
+    <?php tha_footer_before(); ?>
 	<footer id="colophon" class="site-footer" role="contentinfo">
+        <?php tha_footer_top(); ?>
 		<div class="site-info">
 			<?php do_action( '_s_credits' ); ?>
 			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', '_s' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( __( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
 		</div><!-- .site-info -->
+        <?php tha_footer_bottom(); ?>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
-
+<?php tha_footer_after(); ?>
+<?php tha_body_bottom(); ?>
 <?php wp_footer(); ?>
 
 </body>
